a4bec121899d7a73ac45b946f216752dbe6ac004
Kswap Farm <77991984+okexfarm@users.noreply.github.com>
2021-04-18 02:14:15 +0530

    Add files via upload

---
+++ _nuxt/pages/contracts.80346e9.js
@@ -0,0 +1,105 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+    [7], {
+        305: function(t, e, c) {
+            var content = c(400);
+            "string" == typeof content && (content = [
+                [t.i, content, ""]
+            ]), content.locals && (t.exports = content.locals);
+            (0, c(56).default)("cccf6c22", content, !0, {
+                sourceMap: !1
+            })
+        },
+        399: function(t, e, c) {
+            "use strict";
+            c(305)
+        },
+        400: function(t, e, c) {
+            (e = c(55)(!1)).push([t.i, ".intro[data-v-2ea94a50]{background-color:#eee;padding:1rem}.line[data-v-2ea94a50]{margin-top:1rem;margin-bottom:2rem}", ""]), t.exports = e
+        },
+        685: function(t, e, c) {
+            "use strict";
+            c.r(e);
+            c(399);
+            var r = c(41),
+                component = Object(r.a)({}, (function() {
+                    var t = this,
+                        e = t.$createElement,
+                        c = t._self._c || e;
+                    return c("div", [c("div", [c("div", {
+                        staticClass: "intro"
+                    }, [c("h3", {
+                        staticClass: "text-center"
+                    }, [t._v("\n        " + t._s(t.$t("about.contracts")) + "\n      ")])]), t._v(" "), c("div", {
+                        staticClass: "intro"
+                    }, [c("div", [c("i18n", {
+                        attrs: {
+                            path: "about.milk",
+                            tag: "label",
+                            for: "about.milk-address"
+                        }
+                    }, [c("a", {
+                        attrs: {
+                            href: "https://bscscan.com/address/0x0fc64a803745275971fe40028196e9ddd684945b",
+                            target: "_blank"
+                        }
+                    }, [t._v("0x0fc64a803745275971fE40028196E9DDd684945B")])]), t._v(" "), c("i18n", {
+                        attrs: {
+                            path: "about.hdpstaking",
+                            tag: "label",
+                            for: "about.milk-address"
+                        }
+                    }, [c("a", {
+                        attrs: {
+                            href: "https://bscscan.com/address/xxx",
+                            target: "_blank"
+                        }
+                    }, [t._v("XXX")])]), t._v(" "), c("i18n", {
+                        attrs: {
+                            path: "about.hdpbnb",
+                            tag: "label",
+                            for: "about.milk-address"
+                        }
+                    }, [c("a", {
+                        attrs: {
+                            href: "https://bscscan.com/address/xxx",
+                            target: "_blank"
+                        }
+                    }, [t._v("xxx")])]), t._v(" "), c("i18n", {
+                        attrs: {
+                            path: "about.hdpbnbstaking",
+                            tag: "label",
+                            for: "about.milk-address"
+                        }
+                    }, [c("a", {
+                        attrs: {
+                            href: "https://bscscan.com/address/xxx",
+                            target: "_blank"
+                        }
+                    }, [t._v("xxx")])]), t._v(" "), c("i18n", {
+                        attrs: {
+                            path: "about.hdpbusd",
+                            tag: "label",
+                            for: "about.milk-address"
+                        }
+                    }, [c("a", {
+                        attrs: {
+                            href: "https://bscscan.com/address/xxx",
+                            target: "_blank"
+                        }
+                    }, [t._v("xxx")])]), t._v(" "), c("i18n", {
+                        attrs: {
+                            path: "about.hdpbusdstaking",
+                            tag: "label",
+                            for: "about.milk-address"
+                        }
+                    }, [c("a", {
+                        attrs: {
+                            href: "https://bscscan.com/address/xxx",
+                            target: "_blank"
+                        }
+                    }, [t._v("xxx")])])], 1)])])])
+                }), [], !1, null, "2ea94a50", null);
+            e.default = component.exports
+        }
+    }
+]);
