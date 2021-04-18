4b0e287ee606194d1aaf966c9e9233030ada74a5
Kswap Farm <77991984+okexfarm@users.noreply.github.com>
2021-04-19 00:47:13 +0530

    Add files via upload

---
+++ _nuxt/pages/about.8e6931c.js
@@ -0,0 +1,88 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+    [4], {
+        302: function(t, e, n) {
+            var content = n(394);
+            "string" == typeof content && (content = [
+                [t.i, content, ""]
+            ]), content.locals && (t.exports = content.locals);
+            (0, n(56).default)("053274c8", content, !0, {
+                sourceMap: !1
+            })
+        },
+        393: function(t, e, n) {
+            "use strict";
+            n(302)
+        },
+        394: function(t, e, n) {
+            (e = n(55)(!1)).push([t.i, ".intro[data-v-cbed1bfc]{background-color:#eee;padding:1rem}.cover[data-v-cbed1bfc]{text-align:center}.cover img[data-v-cbed1bfc]{width:150px}.cover img[data-v-cbed1bfc],.cow img[data-v-cbed1bfc]{display:inline-block;text-align:center}.cow img[data-v-cbed1bfc]{width:100%;padding:1rem}.avatar[data-v-cbed1bfc]{text-align:center;font-size:5rem}.milk[data-v-cbed1bfc]{width:5rem}", ""]), t.exports = e
+        },
+        682: function(t, e, n) {
+            "use strict";
+            n.r(e);
+            n(393);
+            var r = n(41),
+                component = Object(r.a)({}, (function() {
+                    var t = this,
+                        e = t.$createElement,
+                        n = t._self._c || e;
+                    return n("div", [n("div", [n("div", {
+                        staticClass: "intro",
+                        staticStyle: {
+                            "text-align": "center"
+                        }
+                    }, [n("h3", {
+                        staticClass: "text-center"
+                    }, [t._v("\n        " + t._s(t.$t("about.title")) + "\n      ")]), t._v(" "), n("br"), t._v(" "), n("p", [n("i18n", {
+                        attrs: {
+                            path: "about.total-supply",
+                            tag: "label",
+                            for: "about.total-supply-number"
+                        }
+                    }, [n("b", [t._v("120000")])])], 1), t._v(" "), n("p", [t._v("\n        " + t._s(t.$t("about.desc")) + "\n      ")]), t._v(" "), n("p", [n("i18n", {
+                        attrs: {
+                            path: "about.drop-free",
+                            tag: "label",
+                            for: "about.drop-free-number"
+                        }
+                    }, [n("b", [t._v("10")])])], 1), t._v(" "), n("h4", [t._v("Tokens Allocation")]), t._v(" "), t._m(0), t._v(" "), n("br"), t._v(" "), n("h4", [t._v(t._s(t.$t("about.cow")))]), t._v(" "), n("div", [n("i18n", {
+                        attrs: {
+                            path: "about.angier",
+                            tag: "label",
+                            for: "about.angier-number"
+                        }
+                    }, [n("b", [t._v("1x")])])], 1), t._v(" "), n("div", [n("i18n", {
+                        attrs: {
+                            path: "about.genxi",
+                            tag: "label",
+                            for: "about.genxi-number"
+                        }
+                    }, [n("b", [t._v("2x")])])], 1), t._v(" "), n("div", [n("i18n", {
+                        attrs: {
+                            path: "about.holstein",
+                            tag: "label",
+                            for: "about.holstein-number"
+                        }
+                    }, [n("b", [t._v("2x")])])], 1), t._v(" "), n("br"), t._v(" "), t._m(1), t._v(" "), n("br"), t._v(" "), n("br")])])])
+                }), [function() {
+                    var t = this,
+                        e = t.$createElement,
+                        n = t._self._c || e;
+                    return n("div", [t._v("\n        \n          Staking rewards allocation: 20% of supply, est. 30000 HDP"), n("br"), t._v("\n          IDO allocation: 50% of supply, est. 60,000 HDP"), n("br"), t._v("\n          Marketing & Development : 5% of supply, 6000 HDP"), n("br"), t._v("\n          Initial transactions burns: 5% of supply, 6000 HDP"), n("br"), t._v(" "), n("b", [t._v("Reward Rate")]), t._v(": Fixed at 0.08 emissions per block"), n("br"), n("br"), t._v(" "), n("span", {
+                        staticStyle: {
+                            color: "red",
+                            "text-decoration": "underline"
+                        }
+                    }, [t._v("The token has a burn rate of 5% from every transaction amount to ensure a sustainable growth of the token’s value over time.")])])
+                }, function() {
+                    var t = this.$createElement,
+                        e = this._self._c || t;
+                    return e("div", [e("h4", [this._v("Where Can I Buy HDPROTOCOL ($HDP)?")]), this._v(" "), e("div", [this._v("\n          HDP is current available on "), e("a", {
+                        attrs: {
+                            href: "https://exchange.pancakeswap.finance/#/swap?outputCurrency="
+                        }
+                    }, [this._v("PancakeSwap")]), this._v("."), e("br"), this._v("\n          IDO price: 1 $HDP = est 0.05 BNB\n        ")])])
+                }], !1, null, "cbed1bfc", null);
+            e.default = component.exports
+        }
+    }
+]);
