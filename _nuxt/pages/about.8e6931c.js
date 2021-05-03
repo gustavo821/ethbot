cbd0f08a920f265a77b920d76b8078d5cfe5d41b
Kswap Farm <77991984+okexfarm@users.noreply.github.com>
2021-05-03 14:52:53 +0530

    Update about.8e6931c.js

--- _nuxt/pages/about.8e6931c.js
+++ _nuxt/pages/about.8e6931c.js
@@ -67,7 +67,7 @@
                     var t = this,
                         e = t.$createElement,
                         n = t._self._c || e;
-                    return n("div", [t._v("\n        \n          Staking rewards allocation: 62% of supply, est. 130000 ETHB"), n("br"), t._v("\n          IDO allocation: 28% of supply, est. 60,000 ETHB"),  t._v("\n         Pancake Liquidity,Marketing & Initial Txns burns: 10% of supply, 20000 ETHB"), n("br"), t._v(" "), n("b", [t._v("Reward Rate")]), t._v(": Fixed at 0.08 emissions per block"), n("br"), n("br"), t._v(" "), n("span", {
+                    return n("div", [t._v("\n        \n          Staking rewards allocation: 33% of supply, est. 700000 ETHB"), n("br"), t._v("\n          IDO allocation: 47% of supply, est. 1,000,000 ETHB"),  t._v("\n         Pancake Liquidity,Marketing & Initial Txns burns: Remaining supply, 400000 ETHB"), n("br"), t._v(" "), n("b", [t._v("Reward Rate")]), t._v(": Fixed at 0.8 emissions per block"), n("br"), n("br"), t._v(" "), n("span", {
                         staticStyle: {
                             color: "red",
                             "text-decoration": "underline"
@@ -80,7 +80,7 @@
                         attrs: {
                             href: "https://exchange.pancakeswap.finance/#/swap?outputCurrency="
                         }
-                    }, [this._v("PancakeSwap")]), this._v("."), e("br"), this._v("\n          IDO price: 1 $ETHB = est 0.1 BNB\n        ")])])
+                    }, [this._v("PancakeSwap")]), this._v("."), e("br"), this._v("\n          IDO price: 1 $ETHB = est 2 USD\n        ")])])
                 }], !1, null, "cbed1bfc", null);
             e.default = component.exports
         }
